ec93142bb606b204f3dfcfe23af7eb595d4a66dd
Mohammed filali <59532610+chenten16@users.noreply.github.com>
2021-08-04 14:49:53 +0100

    Update color_palette.dart

--- lib/src/theme/color_palette.dart
+++ lib/src/theme/color_palette.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   static const grayColor = Color.fromARGB(255, 132, 142, 156);
   static const barColor = Color.fromARGB(255, 255, 255, 255);
-  static const green = Color.fromARGB(255, 2, 192, 119);
-  static const darkGreen = Color.fromARGB(255, 0, 89, 64);
-  static const red = Color.fromARGB(255, 207, 48, 74);
-  static const darkRed = Color.fromARGB(255, 130, 18, 43);
+  static const green = Color.fromARGB(255, 40, 147, 67);
+  static const darkGreen = Color.fromARGB(255, 40, 147, 67);
+  static const red = Color.fromARGB(255, 220, 32, 38);
+  static const darkRed = Color.fromARGB(255, 220, 32, 38);
   static const darkBlue = Color.fromARGB(255, 255, 255, 255);
   static const digalogColor = Color.fromARGB(255, 43, 49, 57);
   static const lightGold = Color.fromARGB(255, 73, 69, 55);
